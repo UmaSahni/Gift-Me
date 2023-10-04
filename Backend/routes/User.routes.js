1643c250044e8e84381fd33ce52ff51aceeab20b
Uma Sahni <monkeytown64@gmail.com>
2023-10-05 01:00:27 +0530

    While login sending token

--- Backend/routes/User.routes.js
+++ Backend/routes/User.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { UserModel } = require("../Model/user.model");
 const bcrypt = require("bcrypt");
 const userRouter = express.Router();
-
+const jwt = require('jsonwebtoken');
 userRouter.post("/signup", async (req, res) => {
   const { name, email, password } = req.body;
 
@@ -22,9 +22,29 @@ userRouter.post("/signup", async (req, res) => {
   }
 });
 
-userRouter.post("/login", (req, res) => {
+userRouter.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await UserModel.findOne({email})
+    console.log(user)
+    if(user) {
+        bcrypt.compare(password, user.password, function(err, result) {
+              if(result) {
+                const token = jwt.sign({ foo: 'bar' }, "masai");
+                console.log(token)
+                res.send({"Data":"Login success", token})
+            }
+            else{
+              res.send("Password is wrong")
+            }
+        });
+    }
+    else{
+        res.send({"data":"Please register yourself first"})
+    }
+  } catch (error) {
 
-    
+  }
 });
 
 module.exports = { userRouter };
