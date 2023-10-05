2939a8ff64d8ad4c729a70ee7b66d0be90abe22d
Uma Sahni <monkeytown64@gmail.com>
2023-10-05 16:33:34 +0530

    After redux

---
+++ fronted/src/Redux/AuthReducer/reduder.js
@@ -0,0 +1,15 @@
+// import { FAILURE_LOGIN, REQUEST_LOGIN, SUCCESS_LOGIN } from "./actionTypes"
+
+const initialState = {
+    isLoading : false,
+    isError: false,
+    isAuth: false,
+    token:""
+}
+
+export const reducer = (state = initialState , {type, payload}) => {
+switch(type){
+   
+    default : return state
+}
+}
